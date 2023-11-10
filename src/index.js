6d2f7be8a8c96ec27d8d29d39eb50a97e8c12a9d
GemmaG7 <gemmagrat921@gmail.com>
2023-11-10 14:20:39 +0000

    display issue

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "font-awesome/css/all.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 
 import App from "./App";
