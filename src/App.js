6862d284f31b37b4384f9d590c0266c93040c9fd
GemmaG7 <gemmagrat921@gmail.com>
2023-11-10 12:04:20 +0000

    installing libraries

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import LandingPage from "./Components/LandingPage/LandingPage";
 import Property from "./Components/Property/Property";
 import Seller from "./Components/Seller/Seller";
 import Buyer from "./Components/Buyer/Buyer";
-import SellerProperty from "./Components/Seller/SellerProperty";
+//import SellerProperty from "./Components/Seller/SellerProperty";
 import Booking from './Components/Booking/Booking';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
                     <Route index element={<Property/>}/> {/*This is the default component shown in the <Outlet> tag */}
                     <Route path="property" element={<Property/>}/>
                     <Route path="seller" element={<Seller/>}/>
-                    <Route path="seller/:sellerId/property" element={<SellerProperty/>}/>
+                    {/*<Route path="seller/:sellerId/property" element={<SellerProperty/>}/> */}
                     <Route path="buyer" element={<Buyer/>}/>
                     <Route path="property/:propertyId/booking" element={<Booking/>}/>
                     {/*<Route path="*" element={<NoPage/>}/>*/}
