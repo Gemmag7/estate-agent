1878a6d9b560729358f95c04f2c81627219a95cc
GemmaG7 <gemmagrat921@gmail.com>
2023-11-14 14:08:59 +0000

    Using header.css in header.js file

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,15 @@
 import {Link, Outlet} from "react-router-dom";
-import '../LandingPage/LandingPage.css';
+import './Header.css';
 import React from "react";
 import Navigation from "../Navigation/Navigation";
 
 function Header() {
     return (
         <>
-        <div className="fixed-top container" style={{backgroundColor: "white"}}>
+        <div className="fixed-top container" style={{backgroundColor: "white"}}> 
                 <div className="header">
                 <Link to="/" className="col-sm block">
-                        &nbsp;Estate Agent 2
+                        Estate Agent 2
                     </Link>
                     </div>
                <Navigation/>
