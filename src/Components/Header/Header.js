0ddcaf9bc5e41cd871989bc1b633e6988c6e4f7d
GemmaG7 <gemmagrat921@gmail.com>
2023-11-14 15:25:28 +0000

    Removed unsued imports

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import {Link, Outlet} from "react-router-dom";
+import {Link} from "react-router-dom";
 import './Header.css';
 import React from "react";
 import Navigation from "../Navigation/Navigation";
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import {Link, Outlet} from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import './LandingPage.css';
 import '../Header/Header.css'
 import React from "react";
