206dcb0cb41859cc7bd27fd02e3e66a27de8387b
GemmaG7 <gemmagrat921@gmail.com>
2023-11-15 10:53:29 +0000

    Updated navigation layout - can be resized

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,26 +1,77 @@
-import {Link} from "react-router-dom";
+import {BrowserRouter, Route, Routes,Link} from "react-router-dom";
 import './Navigation.css';
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import LandingPage from "../LandingPage/LandingPage";
+import Seller from "../Seller/Seller";
+import {Button} from "../Button/Button"
+import Property from "../Property/Property";
+import Buyer from "../Buyer/Buyer";
+import Booking from "../Booking/Booking";
+import SellerProperty from "../Seller/SellerProperty";
+export default function Navigation() {
 
-function Navigation() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMoileWindow =() => setClick(false);
+    const showButton =() => {
+        if (window.innerWidth <= 960){
+            setButton(false)
+        }
+        else{
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
     return (
-        <>
+        
             
-                <nav className="row">
-                    <Link to="/property" className="col-sm nav-menu">
-                        <i className="bi bi-house-fill"></i>&nbsp;Properties
-                    </Link>
-                    <Link to="/seller" className="col-sm nav-menu">
-                        <i className="bi bi-person-square"></i>&nbsp;Sellers
-                    </Link>
-                    <Link to="/buyer" className="col-sm nav-menu">
-                        <i className="bi bi-basket3-fill"></i>&nbsp;Buyers
-                    </Link>
-                </nav>
+                <div className="container">
+                    <nav className="navbar">
+                        <div className="navbar-container"> 
+                        <Link to="/" className="nabar-logo" onClick={closeMoileWindow}> Estate Agent</Link>
+                        <div className="menu-icon" onClick={handleClick}>
+                            <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                        </div>   
+                        <ul className={click ? 'nav-menu active' : 'nav-menu '}>
+                            <li className="nav-item">
+                                <Link to='/properties' className="'nav-links" onClick={closeMoileWindow}>
+                                    Properties
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/sellers' className="'nav-links" onClick={closeMoileWindow}>
+                                    Sellers
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/buyers' className="'nav-links" onClick={closeMoileWindow}>
+                                    Buyers
+                                </Link>
+                            </li>
+                        </ul>
+                        </div>
+                    </nav>  
+                    <Route exact path="/"><LandingPage/></Route>
+                    <Route index element={<Property/>}/> {/*This is the default component shown in the <Outlet> tag */}
+                    <Route path="property" element={<Property/>}/>
+                    <Route path="seller" element={<Seller/>}/>
+                    <Route path="seller/:sellerId/property" element={<SellerProperty/>}/>
+                    <Route path="buyer" element={<Buyer/>}/>
+                    <Route path="property/:propertyId/booking" element={<Booking/>}/>
+                    <Route path="/:id"></Route>
+                </div> 
+                    
+                
                 
             
             
-        </>
+        
         );
     }
-export default Navigation;
