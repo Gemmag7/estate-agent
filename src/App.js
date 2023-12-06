ee5dea7b91e020de6249819d8ed6d3ef33fd137c
GemmaG7 <gemmagrat921@gmail.com>
2023-12-06 13:34:05 +0000

    Changing more routes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage/>}> {/*This is the root path*/}
                     <Route index element={<ViewProperties/>}/> {/*This is the default component shown in the <Outlet> tag */}
-                    <Route path="property" element={<Property/>}/>
+                    <Route path="property" element={<ViewProperties/>}/>
                     <Route path="/property/add" element={<AddProperty/>}/>
                     <Route path ="/seller" element={<ViewSellers/>}/>
                     <Route path ="/seller/:id" element={<DeleteSeller/>}/>
@@ -32,7 +32,7 @@ function App() {
                     <Route path="buyer" element={<Buyer/>}/>
                     {/**<Route path ="/buyer/:id/edit" element={<EditBuyer/>}/>*/}
                    {/**  <Route path ="/buyer/add" element={<AddBuyer/>}/>*/}
-                    <Route path="/viewproperty" element={<ViewProperties/>}/>
+                    
                     <Route path="/property/:id/withdraw" element={<WithdrawProperty/>}/>
                     <Route path="/property/:propertyId/booking" element={<Booking/>}/>
                     <Route path="/property/:id/edit" element={<EditProperty/>}/>
